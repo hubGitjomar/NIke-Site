58c731733fb03030db3a6f665d14a2048adef669
hubGitjomar <actjomar@gmail.com>
2025-04-22 09:37:01 +0800

    first commit

---
+++ my-app/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <>
+      <div className="navbar-container">
+        <div>
+          <h3>Nike</h3>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
